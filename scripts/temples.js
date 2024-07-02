6a5c02ff215a8d9df66e11e466828442f9c6b586
cristiangsmiranda <cristiangsmiranda@gmail.com>
2024-07-02 16:41:03 -0300

    temples added

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,21 +1,18 @@
-// Populate the current year
-document.getElementById("currentyear").textContent = new Date().getFullYear();
+const hamButton = document.querySelector("#menu");
+const navigation = document.querySelector(".navigation");
 
-// Populate the last modified date
-document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
+hamButton.addEventListener("click", () => {
+    navigation.classList.toggle("open");
+    hamButton.classList.toggle("open");
 
-const hamButton = document.querySelector('#menu');
-const navigation = document.querySelector('.navigation');
-
-hamButton.addEventListener('click', () => {
-    navigation.classList.toggle('open');
-    hamButton.classList.toggle('open');
-
-    // Ajusta o foco para o primeiro link ao abrir o menu
-    if (navigation.classList.contains('open')) {
-        const firstLink = navigation.querySelector('a');
+    if (navigation.classList.contains("open")) {
+        const firstLink = navigation.querySelector("a");
         if (firstLink) {
             firstLink.focus();
         }
     }
 });
+
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
