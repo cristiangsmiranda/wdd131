f29f7e114d4eeb3595b0136ef3cb3cb3dc759e37
cristiangsmiranda <cristiangsmiranda@gmail.com>
2024-07-22 22:27:23 -0300

    Update added

--- project/scripts/cars.js
+++ project/scripts/cars.js
@@ -2,33 +2,40 @@ const cars = [
     {
         Title: "car wrapping",
         imageUrl:
-      "https://cdn.gencraft.com/prod/user/d72117ed-5924-46e2-b495-8bf9a3e37da4/96ba5081-67ff-492c-bac4-d7e16a68af76/image/image1_0.jpg?Expires=1721279174&Signature=qPfEYZ~gi-dtJD8oPSfnbzFOuqpm39XCmpwjPdw-HP5Iznzllv9hkV5m1qfaDv7bl7T3dnVB4zAWuqdiY6-7aln8pQq8GScT7PkL~4wjVmexaUE2vAKBidYdE7PsLRI3ps5AcDxbuDhN3zHjuu1-nglmQbcwhJdj5zapiizL-WdpT1zVQCS8vPjfk0RTeWTv5z2awqQ5Y-qhqdjHCJzVC~M~Z8M1MQRl9lu8mDBjYV-xOGufncy5KsdgpVtqyNRD4AvomaxLHroMLwMLcw3UboKr~RdDhY6cuRSm7WQRmhD3vNY826ughKvNPeNQBHheiFlxcamY9M2GpHdAKdkuVw__&Key-Pair-Id=K3RDDB1TZ8BHT8"
-    },
+        "https://cdn.gencraft.com/prod/user/d72117ed-5924-46e2-b495-8bf9a3e37da4/c93aecb6-18c0-4a03-a169-9fe010615bd3/image/image0_0.jpg?Expires=1721783350&Signature=o2SPPRuBufU3bY-zsZEPiKdnItoHiJVNhVd1GkY75UK4zYZfKWvWry1V-VT5RGVYji1kcbTDKjugUaUiRZkiniUcfmu9j2L4qiwmomjfWseBVfLnrkkjZI0DRDD5gTNDVwKk1slNnD56V0CzQqbpATB5~LLm-laGyd4~syLZqyGLz4pG6pA~eSD0O-KRGBUK~aGID850Q4w32dMF~xu7dg4K4GTtCtJumN1JVA9pAeRZPRE1sny-qFgPg0JZ9XjUMiucF-hEfVoX4OdH4REtqRZRizIEsfBUEEwKWOkPQodhvMleCWLCWbPFfRJVyrdPzdkSb9gUt-CH68UGAHC4tA__&Key-Pair-Id=K3RDDB1TZ8BHT8"},
     {
         Title: "car wrapping",
         imageUrl:
-        "https://cdn.gencraft.com/prod/user/d72117ed-5924-46e2-b495-8bf9a3e37da4/69505975-7f1e-4aca-8d37-9fbcc8b5503b/image/image1_0.jpg?Expires=1721279039&Signature=KdC0HBbQq8WMySvhMc9uFmBgun-u4sSLy4r4I6Yn3Jlkj6J-12lGSc09BX0IpYlis4SyyFsXxtghFX-7N4~ruwqFEWWlaHY~dmOtfMqdE9XEqd4Tg9De9o4ex4T~3JBNQds7-Rtn-iZlQkdZi0NLYZ8hjCL6pt2mTJ3v44g5Khb2Z6IuRZq~0rq16jrwXnl4LUvSK35cM7MvGMgm3xCkU-H-f0waBgeoE~lgFWE6kvBtXMoNoZISAHv4-Z2dDHIHRAfoVzZ7C~FWvQ-J2ruCRl1NU2-z3PECjE4V1IWv35oiNxGLnfjg5p~jDH-k9gwfhqxNn4jCjyot0LPuKTL8ww__&Key-Pair-Id=K3RDDB1TZ8BHT8"
-    },
+        "https://cdn.gencraft.com/prod/user/d72117ed-5924-46e2-b495-8bf9a3e37da4/5e06634f-7766-4703-9eb2-03ee6ec4ed16/image/image0_0.jpg?Expires=1721783235&Signature=blrAA-pgjvotMU0R9ZJzpdIDKyOrA7LDPd1ZlbvOk4vEtLW6pFrlwSipL1TAleSLqj8TpI0q~4ZFAG5OO38abf~23F8dDY6VHCcw8UCxB-LJCzmm3POeinLYeDMDn4LVgDLQ4~~FhWtHewGXT3siN-glMJmFKimljFJHrNchcRrlibZaLqG~zrPeSMHypHOwzwCt1s9ujIzAzD3qCO-1BP79SniQnsKnCguxPCjMgpZj0T4CDj994q76Iyi~1bjjImbqVhNyiEPgETNWJvOgOeD1y9a3Ps18atoEHyQMlLoY27qbGjerm~9it-FPGr~ZCBFG7~b6iogQtHUO5lisag__&Key-Pair-Id=K3RDDB1TZ8BHT8"},   
     {
         Title: "car wrapping",
         imageUrl:
-        "https://cdn.gencraft.com/prod/user/d72117ed-5924-46e2-b495-8bf9a3e37da4/96ba5081-67ff-492c-bac4-d7e16a68af76/image/image0_0.jpg?Expires=1721279174&Signature=kbToWIU8voyYo6Gq4e3X7Xh13xHNr6ix4rFEZzrggxrQJNzFC4JrxOTkkl7vwdBDNS1QQn4mUgCMo84SC50qfhCU7cBFcbzjgnCHSNfFKk~ClNxCvBwPHDbyuQa-HKw~K3LWdTc9prbMMjR10a1bKw-oGRsTtpDtox9EviUhDpU6rJeOh27P3lXM3Z8jxyJh7DlQzHENh4qnag8fjIGy-nTUWBW3C0FOufWHKlJVtngSwQR2ZqHguGwRVw7qlEqq-HY0sKQ11GKcBwV6cu9tUMJ8t3lQmbAyRtbaLhLg~6d9r4IVzWRW2mw1ztxzRDTErpIT25AwM2lU4HeerJuiOw__&Key-Pair-Id=K3RDDB1TZ8BHT8"
-    },
+        "https://cdn.gencraft.com/prod/user/d72117ed-5924-46e2-b495-8bf9a3e37da4/c93aecb6-18c0-4a03-a169-9fe010615bd3/image/image1_0.jpg?Expires=1721783350&Signature=lYw1KX18gwHbasgV2kP6SwZwP~2jPXFONLwSqtnQOdnY-oxUq8LIKcQsvgnbrMV3wKF17kAT5c85dNSBQ79tJHMm4Qdpaw65xlh46h-q18BMrEahSAaK1Irp0fpKJuWvNtCOjr3psPNHGHgHuQaCD-xiTwAmG4rqdylNvdtQvPWlFQsD1D1nKUf6BPJzQc~xG8SjLCvxJYKJKBUaG0Vf0y5QitnSCLkXrNRKBeW3pkWvfbZg2LsXPESFQ9vvLI1gQZqP725Z-wrxaSkDnm6NWz4G6WadXhPSv3hgZ4Yalil1kzMfiI6eIoZeeSzIg5KmN9xIYtXUcNjdbW9x93VblQ__&Key-Pair-Id=K3RDDB1TZ8BHT8"},
     {
         Title: "car wrapping",
         imageUrl:
-        "https://img.freepik.com/fotos-gratis/pessoa-trabalhando-no-envelopamento-de-carros_23-2149342664.jpg?t=st=1721191869~exp=1721195469~hmac=7634c406e60e702f2fe500415a5ef0843540be346c4f575198f75869cd3c0f65&w=826"
-    },
+        "https://img.freepik.com/fotos-gratis/salao-de-servico-automatico-doign-embrulho-de-carro_23-2149593831.jpg?t=st=1721191604~exp=1721195204~hmac=7b0fdb0637262ec02a065f4d1fdee531dc81e6f3bf6ef45428d500714ee1870b&w=360"},
     {
         Title: "car wrapping",
         imageUrl:
-        "https://img.freepik.com/fotos-gratis/salao-de-servico-automatico-doign-embrulho-de-carro_23-2149593881.jpg?t=st=1721191796~exp=1721195396~hmac=d420067dcd98819734ccfb2aa1e59818152cf989b7f19064681f250ff2f66b54&w=826"
-    },
+        "https://img.freepik.com/fotos-gratis/carro-amarelo-preto-e-roda-de-auto-ajuste_114579-4036.jpg?t=st=1721191694~exp=1721195294~hmac=e707f2757a50368ffda9fd7e0311abb6f155cd1959ad9f05575864d91b72427e&w=360"},
+    {
+        Title: "car wrapping",
+        imageUrl:
+        "https://img.freepik.com/fotos-gratis/luzes-dianteiras-do-xenon-de-um-carro-preto-amarelo-vista-do-topo_114579-5061.jpg?t=st=1721191637~exp=1721195237~hmac=f4d1a1287dfc5a0f1fb9f86328f8bd9185a773118a4a8087ab6056ef07bad6cd&w=360"},
+    {
+        Title: "car wrapping",
+        imageUrl:
+        "https://img.freepik.com/fotos-gratis/pessoa-trabalhando-no-envelopamento-de-carros_23-2149342664.jpg?t=st=1721191869~exp=1721195469~hmac=7634c406e60e702f2fe500415a5ef0843540be346c4f575198f75869cd3c0f65&w=826"},
+    {
+        Title: "car wrapping",
+        imageUrl:
+        "https://img.freepik.com/fotos-gratis/salao-de-servico-automatico-doign-embrulho-de-carro_23-2149593881.jpg?t=st=1721191796~exp=1721195396~hmac=d420067dcd98819734ccfb2aa1e59818152cf989b7f19064681f250ff2f66b54&w=826"},
     {
         Title: "car wrapping",
         imageUrl:
         "https://img.freepik.com/fotos-gratis/envelopamento-de-carro-com-papel-aluminio-no-servico-de-carro_1303-32341.jpg?t=st=1721192260~exp=1721195860~hmac=dbbb6d16433117c3ccc8242cce651389fee385da6595c905ac56833e4ef68757&w=740"
-    },
+    }
 ];
 
 document.addEventListener("DOMContentLoaded", () => {
